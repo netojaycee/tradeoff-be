37390905107b5fe5b206d1b1874a8ddcaac47825
John Chinonso Edeh <netojaycee@gmail.com>
2025-11-19 22:30:14 +0100

    G

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get()
+  @HttpCode(200)
+  @Header('Cache-Control', 'no-cache')
+  root() {
+    return { status: 'ok', service: 'Tradeoff Fashion Marketplace API' };
   }
 }
